b80547c6b18023a26617caefbf2e5712b6965f92
jaranike <manueljv7@gmail.com>
2022-02-18 13:20:29 +0100

    Modificaciones realizadas en la clase del viernes 18 febrero

---
+++ html/jaranay.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Jaranay</title>
+    </head>
+    <body>
+
+        <?php
+            echo "¡Hola, soy Jaranay!";
+        ?>
+    </body>
+</html>
